b9a013fdfbc33ed7f25470df2112908c587abd8c
yorm-876 <alex.tambo.15432@gmail.com>
2024-11-20 19:17:21 +0100

    added ft_malloc

--- Makefile
+++ Makefile
@@ -50,6 +50,7 @@ SRCS = ft_atoi.c\
        ft_putstr_fd.c\
        ft_putendl_fd.c\
        ft_putnbr_fd.c\
+       ft_malloc.c \
 
 
 OBJS = $(SRCS:.c=.o)
--- libft.h
+++ libft.h
@@ -25,7 +25,6 @@ int		ft_isalnum(char c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 void	ft_putchar_fd(char c, int fd);
-
 // String
 int		ft_strlen(char const *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -48,7 +47,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
 // Memory
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
@@ -57,5 +55,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_malloc(size_t size);
 
 #endif
