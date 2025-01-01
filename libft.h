aba48968e552e74e96e440df0612a40eee200ca3
atambo <alex.tambo.15432@gmail.com>
2025-01-01 17:13:55 +0100

    Edits .h a little

--- libft.h
+++ libft.h
@@ -24,7 +24,7 @@ int		ft_isdigit(char c);
 int		ft_isalnum(char c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-void	ft_putchar_fd(char c, int fd);
+void		ft_putchar_fd(char c, int fd);
 // String
 int		ft_strlen(char const *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -37,7 +37,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	**ft_split(const char *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -54,7 +53,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_malloc(size_t size);
 void	ft_free(void **ptr);
 
