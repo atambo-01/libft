771d69c69481e96dbea6f636a91a441320f42951
alex tambo <alex.tambo.15432@gmail.com>
2025-01-18 07:57:39 +0100

    Changes ft_strcpy back to original from

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,21 +6,16 @@
 /*   By: atambo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 07:44:12 by atambo            #+#    #+#             */
-/*   Updated: 2025/01/10 19:30:07 by atambo           ###   ########.fr       */
+/*   Updated: 2025/01/18 07:56:42 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-char	*ft_strcpy(char *src)
+char	*ft_strcpy(char *src, char *dest)
 {
-	int		i;
-	char	*dest;
-
 	if (!src)
 		return (NULL);
-	i = 0;
-	dest = malloc(sizeof(char) * ft_strlen(src) + 1);
 	while (src[i])
 	{
 		dest[i] = src[i];
